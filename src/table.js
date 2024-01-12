a15159c2e351135839e8f328ab8626dca743d94b
fred <frederico666@yandex.ru>
2024-01-12 13:01:09 +0400

    remake table

--- src/table.js
+++ src/table.js
@@ -1,44 +1,78 @@
+import React from "react";
+import { useTable } from 'react-table';
+import DateObject from "react-date-object";
 
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+const date = new DateObject();
 
-function createData(date, payment) {
-    return { date, payment };
-}
-
-const rows = [
-    createData('23/12/1990', 23000)
+const Columns = [
+    {
+        Header: "Date",
+        accesor: "date",
+        input: "date",
+    },
+    {
+        Header: "Payment",
+        accesor: "payment",
+        input: "number",
+    },
 ];
 
-export default function BasicTable() {
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Payment</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
 
-                            <TableCell>{row.date}</TableCell>
-                            <TableCell>{row.payment}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import * as React from 'react';
+// import Table from '@mui/material/Table';
+// import TableBody from '@mui/material/TableBody';
+// import TableCell from '@mui/material/TableCell';
+// import TableContainer from '@mui/material/TableContainer';
+// import TableHead from '@mui/material/TableHead';
+// import TableRow from '@mui/material/TableRow';
+// import Paper from '@mui/material/Paper';
+//
+// function createData(date, payment) {
+//     return { date, payment };
+// }
+//
+// const rows = [
+//     createData('23/12/1990', 23000)
+// ];
+//
+// export default function BasicTable() {
+//     return (
+//         <TableContainer component={Paper}>
+//             <Table>
+//                 <TableHead>
+//                     <TableRow>
+//                         <TableCell>Date</TableCell>
+//                         <TableCell>Payment</TableCell>
+//                     </TableRow>
+//                 </TableHead>
+//                 <TableBody>
+//                     {rows.map((row) => (
+//                         <TableRow
+//                             key={row.name}
+//                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+//                         >
+//
+//                             <TableCell>{row.date}</TableCell>
+//                             <TableCell>{row.payment}</TableCell>
+//                         </TableRow>
+//                     ))}
+//                 </TableBody>
+//             </Table>
+//         </TableContainer>
+//     );
+// }
