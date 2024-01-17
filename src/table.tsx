ef1a5aea491d9fb26c4345842afedcfbfee9811a
zhenyafka <zhenyafka163@gmail.com>
2024-01-17 20:11:25 +0400

    Make scrolling table

--- src/table.tsx
+++ src/table.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {FC} from 'react';
+import { makeStyles } from '@material-ui/styles';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -14,29 +15,61 @@ export interface BasicTableProps {
     isHiddenTable: boolean
 }
 
+interface Column {
+    id: 'paymentDate' | 'amountOfPayment';
+    label: string;
+    minWidth?: number;
+    align?: 'right';
+    format?: (value: number) => string;
+}
+
+const columns: Column[] = [
+    {id: 'paymentDate', label: 'Payment Date', minWidth: 170},
+    {id: 'amountOfPayment', label: 'Amount of Payment', minWidth: 170},
+]
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    container: {
+        maxHeight: 600,
+    },
+});
+
 export const BasicTable: FC<BasicTableProps> = ({rows,isHiddenTable}) => {
+    const classes = useStyles();
     return (
         isHiddenTable ? <div/> :
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Payment</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.id}
-                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                        >
-                            <TableCell>{row.dateOfPayment.toDateString()}</TableCell>
-                            <TableCell>{row.oneTimePayment}</TableCell>
+        <Paper className={classes.root}>
+            <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell>{row.dateOfPayment.toDateString()}</TableCell>
+                                <TableCell>{row.oneTimePayment}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
     );
 }
