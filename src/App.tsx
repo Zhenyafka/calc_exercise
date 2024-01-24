ff00db7b13a72327a2a55ee2ca9c481bdad6a90f
zhenyafka <zhenyafka163@gmail.com>
2024-01-24 15:12:39 +0400

    Add sliders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {useState} from "react";
 import "./Components.css"
-import {TextField} from "@mui/material";
+import {FormControl, InputLabel, MenuItem, TextField} from "@mui/material";
 import {BasicTable} from "./table.tsx";
 import DatePicker from "react-datepicker";
-import 'react-datepicker/dist/react-datepicker.css'
+import 'react-datepicker/dist/react-datepicker.css';
+import {React} from "react";
 
 
 export interface Row {
@@ -28,6 +29,8 @@ export const App = () => {
         const base = Math.pow(1 + r, 1 / 12.)
         const oneTimePayments = P * Math.pow(base, n) * (base - 1) / (Math.pow(base, n) - 1)
 
+
+
         const startDay = new Date(date)
         const result: Row[] = []
         for (let i = 0; i < numberOfMonth; i++) {
@@ -38,6 +41,28 @@ export const App = () => {
         return result
     }
 
+    const simplePayment = (creditAmount, interestRate, numberOfMonth) => {
+        const P = creditAmount
+        const r = interestRate / 100.
+        const n = numberOfMonth
+        const total = ()
+    }
+
+    // const calculatePaymentsDiff = (creditAmount, interestRate, numberOfMonth) => {
+    //     const P = creditAmount
+    //     const r = interestRate / 100.
+    //     const n = numberOfMonth
+    //     const diffPayment = P / n + P * r
+    //     const BalanceOwed = P - diffPayment
+    //     const newPayment = new BalanceOwed
+    //
+    //     const startDay = new Date(date)
+    //     const result: Row[] = []
+    //     for (let i = 0; i < numberOfMonth; i++) {
+    //         let nextPaymentMonth: Date = new Date(startDay.setMonth(startDay.getMonth() + 1));//RAZBERIS' 2
+    //         result.push({id: i, dateOfPayment: nextPaymentMonth, oneTimePayment: oneTimePayments} as Row)
+    //     }
+    // }
 
     const calculateFormulaAndShowTable = () => {
 
@@ -51,29 +76,47 @@ export const App = () => {
         setIsHiddenTable(false)
     }
 
+    let x = document.addEventListener('keydown', function (e) {
+        if (e.key === 'Enter') calculateFormulaAndShowTable()
+    })
+
+
     return (
-        <div
-            className="backgroundOfPage">
-            <div className="mainBlock" onKeyDown={(e) => {
-                if (e.key === 'Enter') calculateFormulaAndShowTable()
-            }}>
+
+        <div className="backgroundOfPage" onChange={x}>
+            <div className="mainBlock">
                 <div className="sumBlock">
                     <TextField onChange={event => setCreditAmount(+event.target.value)} value={creditAmount}
                                label={'creditAmount'}/>
+                    <input className="slider" type="range" min={0} max={1000000}
+                           onChange={event => setCreditAmount(+event.target.value)} value={creditAmount}/>
                 </div>
                 <div className="rateBlock">
                     <TextField onChange={event => setInterestRate(+event.target.value)} value={interestRate}
                                label={'interestRate'}/>
+                    <input className="slider" type="range" min={1} max={100}
+                           onChange={event => setInterestRate(+event.target.value)} value={interestRate}/>
                 </div>
                 <div className="termBlock">
                     <TextField onChange={event => setNumberOfMonth(+event.target.value)} value={numberOfMonth}
                                label={'numberOfMonth'}/>
+                    <input className="slider" type="range" min={1} max={1200}
+                           onChange={event => setNumberOfMonth(+event.target.value)} value={numberOfMonth}/>
                 </div>
-                <div className='dateBlock'>
+                <div className="dateBlock">
                     <DatePicker selected={date} onChange={(date) => setDate(date)}/>
                 </div>
+                <div className="selectorBlock">
+                    <select className="selectType">
+                        <option onChange={calculateFormulaAndShowTable}>Annuity payment</option>
+                        <option value={"differentiated"}>Differentiated payment</option>
+                    </select>
+                </div>
                 <div className="buttonBlock">
-                    <button className="paymentsButton" onClick={calculateFormulaAndShowTable} >
+                    <button className="paymentsButton" onClick={calculateFormulaAndShowTable}
+                            onKeyDown={(e) => {
+                                if (e.key === 'Enter') calculateFormulaAndShowTable()
+                            }}>
                         Total
                     </button>
                 </div>
