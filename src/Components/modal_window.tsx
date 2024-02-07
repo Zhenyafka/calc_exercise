b1a2db4d69d7321f8fac140cb423fbf04403f5d5
zhenyafka <zhenyafka163@gmail.com>
2024-02-07 18:41:22 +0400

    Modal Window component moved to a new folder. Added disappearing of modal window after some time

--- src/Components/modal_window.tsx
+++ src/Components/modal_window.tsx
@@ -1,14 +1,11 @@
 import {React} from "react";
-import "./Components.css"
+import "./modal_window.css"
 
 export const Modal = ({active, setActive}) => {
     return (
-        <div className={active ? "modal-active" : "modal"}>
+        <div className={active ? "modal-active" : "modal"} onClick={() => setActive(false)}>
             <div className='modal-content'>
                 <p>Your data has been accepted</p>
-            </div>
-            <div>
-                <button className='modal-button' onClick={() => setActive(false)}>Close</button>
             </div>
         </div>
     )
