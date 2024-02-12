5fa307b2804d77158b4f9a2cb5d36580e52e1d82
zhenyafka <zhenyafka163@gmail.com>
2024-02-12 14:45:02 +0400

    Moved main function to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,6 @@
 import {React, useState} from "react";
 import "./Components.css"
-// import {FormControl, InputLabel, MenuItem, Select, TextField} from "@mui/material";
-// import {BasicTable} from "./table.tsx";
-// import DatePicker from "react-datepicker";
-// import 'react-datepicker/dist/react-datepicker.css';
-// import {listOfSelectedFunction} from "./formulas.tsx";
-// import {dataRecord} from "./data_record.tsx";
-// import {Modal} from "./Components/modal_window.tsx";
 import {MainPage} from "./first_tab.tsx";
-
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
-
-
-
 
 
 const App = () => {
