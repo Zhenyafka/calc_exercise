160dfe8ea5851bf71e1250c99e9d5c977019ef6c
zhenyafka <zhenyafka163@gmail.com>
2024-02-19 14:49:29 +0400

    Removed useless import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {React, useState} from "react";
+import {React} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Components.css";
 import {Navbar} from "./navbar.tsx";
